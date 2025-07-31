4ec41939ed6e4fed5e5d3cd1b14caaf9da88f562
karishmask191 <karishmask191@gmail.com>
2025-07-31 20:40:37 +0530

    Create 06.Abstraction.py

---
+++ 06.Abstraction.py
@@ -0,0 +1,32 @@
+'''
+6. Abstraction
+#Abstraction hides complex details and exposes only essential info. (or)
+#Abstraction is about showing only what’s needed and hiding how it works inside.
+
+Example: Message Sender
+You have a general message sender. Whether it's email or SMS, the user only sees send_message(), not how it works inside.
+
+Example code:
+---------------
+# Define parent class
+class MessageSender:
+    def send_message(self, message):
+        print("Sending message:", message)
+
+# Define child classes
+class EmailSender(MessageSender):
+    pass
+
+class SMSSender(MessageSender):
+    pass
+
+# Create objects and call method
+email = EmailSender()
+sms = SMSSender()
+
+email.send_message("Welcome to our platform!")
+sms.send_message("Your OTP is 1234.")
+
+Output:
+ Sending message: Welcome to our platform!
+ Sending message: Your OTP is 1234.
